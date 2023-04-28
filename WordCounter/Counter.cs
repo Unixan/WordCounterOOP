2cf493742b1e847d3b0cfd8426693a04e4b861f8
Ståle <Stle@DESKTOP-SIRD2AK>
2023-04-28 15:03:15 +0200

    Add project files.

---
+++ WordCounter/Counter.cs
@@ -0,0 +1,43 @@
+﻿namespace WordCounter;
+
+public class Counter
+{
+    private string _text => File.ReadAllText(_path);
+    private string _path;
+    private StreamReader _stream;
+    private int _lineCount;
+    private int _charCount;
+    private string[] words;
+
+    public Counter(string path)
+    {
+        _path = path;
+    }
+
+    public void CountChars()
+    {
+        _charCount = _text.Count(c => char.IsLetter(c));
+        Console.WriteLine(_charCount + " Letters in text");
+    }
+
+    public void CountWords()
+    {
+        words = _text.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine(words.Length + " words in text");
+
+    }
+
+    public void CountLines()
+    {
+        _stream = new StreamReader(_path);
+        using (_stream)
+        {
+           while (_stream.ReadLine() != null)
+            {
+                _lineCount++;
+            }
+        }
+
+        Console.WriteLine(_lineCount + " Lines in text");
+    }
+}
---
+++ WordCounter/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace WordCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter(@"C:\Users\Ståle\source\repos\WordCounter\WordCounter\Text.txt");
+            counter.CountChars();
+            counter.CountLines();
+            counter.CountWords();
+        }
+    }
+}
